1d94a1508dbc9386bc5b0c20d4529e5a925415fe
jigarrdesai <jigar_desai12@hotmail.com>
2015-05-07 07:07:42 +0530

    first commit

---
+++ bubbly_small/src/small.jsx
@@ -0,0 +1,16 @@
+var React = require("react");
+
+var App = React.createClass({
+
+	
+	render() {
+		return (
+			<div>
+				<h1> Hello i am small to watch</h1>
+			</div>
+		);
+	}
+	
+})
+
+React.render(<App/>,document.getElementById('small'));
